7d258ba7f334616d1cfe806bcdede81d05ba79ff
VolodymyrStukanov <volodymyrstukanov@gmail.com>
2025-11-05 00:59:20 +0200

    fix

--- LLMSiteClassifier/Services/MessageQueueService/models/InputMessage.cs
+++ LLMSiteClassifier/Services/MessageQueueService/models/InputMessage.cs
@@ -3,6 +3,6 @@ namespace LLMSiteClassifier.Services.MessageQueueService.models
     public class InputMessage
     {
         public string ReplyTo { get; set; }
-        public string Prompt { get; set; }        
+        public string Prompt { get; set; }
     }
 }
